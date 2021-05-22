0d7740709700b7a2bd22bac0d390d391eea99089
JuanGrajales <inbox1.juan+github@gmail.com>
2021-05-21 20:53:26 -0400

    feat(react-w2): homework steps

---
+++ react/w2-steps-hw.js
@@ -0,0 +1,29 @@
+// EVERYONE - submit one question on the 3-React channel and class channel. Question could be something you already know the answer to, something you have no clue about, or something you want to learn more about.
+
+// How to create a class component
+
+// How to create a state for a class component
+
+// How to update the state of a class component
+
+// How to use a component that is already created
+
+// How to display an array of elements in react
+
+// How to create a prop
+
+// How to give a prop its value
+
+// How to use a prop that was already created
+
+// How to export a function/variable/component/etc so another file can use it
+
+// Difference between export default and just export
+
+// How to import a function/variable/component/etc from another file
+
+// How to install and use a npm package to your react project
+
+// How to create a react app using create-react-app command
+
+// How to start a react app locally
